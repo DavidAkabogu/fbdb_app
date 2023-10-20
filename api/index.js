41cee34239086bc0c98fb82776d0371ad9242f56
DavidAkabogu <Akabogudavid@gmail.com>
2023-10-20 18:52:44 +0100

    user account page

--- api/index.js
+++ api/index.js
@@ -4,20 +4,24 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "kadeco100%";
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
 mongoose.connect(process.env.MONGO_URL);
 
+// browser testing backend point
 app.get("/test", (req, res) => {
-  res.json("test ok");
+  res.json("express. test ok!");
 });
 
+// register backend point
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -33,6 +37,7 @@ app.post("/register", async (req, res) => {
   }
 });
 
+// login backend point
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const userDoc = await User.findOne({ email }).maxTimeMS(20000);
@@ -41,12 +46,12 @@ app.post("/login", async (req, res) => {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
       jwt.sign(
-        { email: userDoc.email, id: userDoc._id },
+        { email: userDoc.email, id: userDoc._id, name: userDoc.name },
         jwtSecret,
         {},
         (err, token) => {
           if (err) throw err;
-          res.cookie("token", token).json("pass ok");
+          res.cookie("token", token).json(userDoc);
         }
       );
     } else {
@@ -54,6 +59,19 @@ app.post("/login", async (req, res) => {
     }
   } else {
     res.json("not found");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json(name, email, _id);
+    });
+  } else {
+    res.json(null);
   }
 });
 
