1dc7e26656315d013bebeceaf46a34c11e69e9ad
DavidAkabogu <Akabogudavid@gmail.com>
2023-11-10 07:46:13 +0100

    new update

--- api/server.js
+++ api/server.js
@@ -19,6 +19,7 @@ const jwtSecret = "kadeco100%";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 app.use("/uploads", express.static(__dirname + "/uploads/"));
 app.use(
   cors({
